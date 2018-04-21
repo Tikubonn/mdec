60b997e19d51dfb8f0eda4e0550af970fcc8b0ac
tikubonn <https://twitter.com/tikubonn>
2018-04-21 19:24:21 +0900

    a compaction function was redefined.

--- auto/src/compacted_mdec.c
+++ auto/src/compacted_mdec.c
@@ -1,6 +1,23 @@
 #include <mdec.h>
+#define max(a, b) ((a)<(b)?(b):(a))
 
-static mint *common_divisor_in (mint *max, mint *min){
+mdec *compacted_mdec (mdec *num){
+  mint *numerator = make_mint(size_mint(num->numerator));
+  mint *denominator = make_mint(size_mint(num->denominator));
+  mdec *numout = make_mdec(0, numerator, denominator);
+  mint *numtmp1 = make_mint(max(
+    size_mint(num->numerator),
+    size_mint(num->denominator)));
+  mint *numtmp2 = make_mint(max(
+    size_mint(num->numerator),
+    size_mint(num->denominator)));
+  compacted_mdec_manually(num, numout, numtmp1, numtmp2);
+  free_mint(numtmp1);
+  free_mint(numtmp2);
+  return numout;
+}
+
+/* static mint *common_divisor_in (mint *max, mint *min){
   mint *numa = copy_mint(max);
   mint *numb = copy_mint(min);
   while (1){
@@ -21,15 +38,15 @@ static mint *common_divisor_in (mint *max, mint *min){
     free_mint(numb);
     numb = tempb;
   }
-}
+} */
 
-static mint *common_divisor (mint *numa, mint *numb){
+/* static mint *common_divisor (mint *numa, mint *numb){
   if (is_greater_mint(numa, numb))
     return common_divisor_in(numa, numb);
   return common_divisor_in(numb, numa);
-}
+} */
 
-mdec *compacted_mdec (mdec *num){
+/* mdec *compacted_mdec (mdec *num){
   if (is_zero_mdec(num)){
     mint *numerator = make_mint_from_int(0);
     mint *denominator = make_mint_from_int(1);
@@ -43,4 +60,4 @@ mdec *compacted_mdec (mdec *num){
     mdec *md = make_mdec(num->sign, numerator, denominator);
     return md;
   }
-}
+} */
