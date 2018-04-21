2ef0f4251988478bdc148ca711138d9380e85711
tikubonn <https://twitter.com/tikubonn>
2018-04-21 23:27:57 +0900

    some temporary function are optimized.

--- tmp/src/sub_tmp_mdec.h
+++ tmp/src/sub_tmp_mdec.h
@@ -17,6 +17,38 @@ size_mint(__ ## var ## _sub_argument2->denominator));\
 make_tmp_mdec(var,\
 __ ## var ## _sub_argument1->sign,\
 __ ## var ## _sub_numerator1,\
+__ ## var ## _sub_denominator1); {\
+make_tmp_mint(__ ## var ## _sub_numerator2,\
+size_mint(__ ## var ## _sub_argument2->numerator)+\
+size_mint(__ ## var ## _sub_argument1->denominator));\
+make_tmp_mint(__ ## var ## _sub_denominator2,\
+size_mint(__ ## var ## _sub_argument2->denominator)+\
+size_mint(__ ## var ## _sub_argument1->denominator));\
+make_tmp_mdec(__ ## var ## _sub_mdec2,\
+__ ## var ## _sub_argument2->sign,\
+__ ## var ## _sub_numerator2,\
+__ ## var ## _sub_denominator2);\
+sub_mdec_manually(\
+__ ## var ## _sub_argument1,\
+__ ## var ## _sub_argument2, var,\
+__ ## var ## _sub_mdec2); }
+
+/* #define sub_tmp_mdec_max(a, b) ((a)<(b)?(b):(a))
+#define sub_tmp_mdec(var, numa, numb)\
+mdec *__ ## var ## _sub_argument1 = numa;\
+mdec *__ ## var ## _sub_argument2 = numb;\
+make_tmp_mint(__ ## var ## _sub_numerator1,\
+sub_tmp_mdec_max(\
+size_mint(__ ## var ## _sub_argument1->numerator)+\
+size_mint(__ ## var ## _sub_argument2->denominator),\
+size_mint(__ ## var ## _sub_argument2->numerator)+\
+size_mint(__ ## var ## _sub_argument1->denominator)) +1);\
+make_tmp_mint(__ ## var ## _sub_denominator1,\
+size_mint(__ ## var ## _sub_argument1->denominator)+\
+size_mint(__ ## var ## _sub_argument2->denominator));\
+make_tmp_mdec(var,\
+__ ## var ## _sub_argument1->sign,\
+__ ## var ## _sub_numerator1,\
 __ ## var ## _sub_denominator1);\
 make_tmp_mint(__ ## var ## _sub_numerator2,\
 size_mint(__ ## var ## _sub_argument2->numerator)+\
@@ -31,4 +63,4 @@ __ ## var ## _sub_denominator2);\
 sub_mdec_manually(\
 __ ## var ## _sub_argument1,\
 __ ## var ## _sub_argument2, var,\
-__ ## var ## _sub_mdec2);
+__ ## var ## _sub_mdec2); */
